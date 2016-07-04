bfb1804e08d8a4451d7ff82f32fe8eed477cbe6d
msaitoo <m.saito017@gmail.com>
2016-07-04 14:43:18 +0900

    travels from bottom line

--- solver.py
+++ solver.py
@@ -13,52 +13,53 @@ numberofcities = len(dat)
 def distance(city1, city2):
     return numpy.sqrt((city1[0] - city2[0]) ** 2 + (city1[1] - city2[1]) ** 2)
 
-def rearrangeCities(dat):
-    xaxis =[]
+def divideaxis(data):
+    xaxis = []
     yaxis = []
     xindex = []
     yindex = []
-    for i in range(numberofcities):
+    for i in range(len(data)):
         xaxis.append(dat[i][0])
         xindex.append(i)
         yaxis.append(dat[i][1])
         yindex.append(i)
+    return (xaxis, yaxis)
+
+def lowline (xaxis, yaxis, maxx, yindex, ordered):
+    while xaxis[yindex] < maxx:
+        ordered.append([xaxis[yindex],yaxis[yindex]])
+        xaxis.pop(yindex)
+        yaxis.pop(yindex)
+        miny = min(yaxis)
+        yindex = yaxis.index(miny)
+    return (ordered, xaxis, yaxis)
+
+def rearrangeCities(dat):
+    xaxis = divideaxis(dat)[0]
+    yaxis = divideaxis(dat)[1]
     
-    order = []
-    minx = min(xaxis)
-    mindex = xaxis.index(minx)
-    order.append([xaxis[mindex],yaxis[mindex]])
+    ordered = []
+    mindex = xaxis.index(min(xaxis))
+    ordered.append([xaxis[mindex],yaxis[mindex]])
     xaxis.pop(mindex)
     yaxis.pop(mindex)
     
     maxx = max(xaxis)
     yindex = yaxis.index(min(yaxis))
+    low = lowline(xaxis, yaxis, maxx, yindex, ordered)
     
-    while xaxis[yindex] < maxx:
-        miny = min(yaxis)
-        yindex = yaxis.index(miny)
-        order.append([xaxis[yindex],yaxis[yindex]])
-        if xaxis[yindex] == maxx:
-            break
-        xaxis.pop(yindex)
-        yaxis.pop(yindex)
+    xaxis = low[1]
+    yaxis = low[2]
+    ordered = low[0]
     
-    while len(order) < numberofcities:
+    while len(ordered) < numberofcities:
         xmax = max(xaxis)
         maxdex = xaxis.index(xmax)
-        order.append([xaxis[maxdex], yaxis[maxdex]])
+        ordered.append([xaxis[maxdex], yaxis[maxdex]])
         xaxis.pop(maxdex)
         yaxis.pop(maxdex)
     
-    #while len(order) < numberofcities:
-        #xmax = max(xaxis)
-    
-    #while len(order) < numberofcities:
-        #minx = min(xaxis)
-        #index = xaxis.index(minx)
-        #xorder.append(dat[index])
-        #xaxis[index] = max(xaxis)+1
-    return order
+    return ordered
 
 def connectCities(xorder):
     traveldistance = 0
