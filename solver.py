2d8a445ae4a550e37f446c03ecc39b41d0c7f218
msaitoo <m.saito017@gmail.com>
2016-07-05 18:37:57 +0900

    goes around the way I want it to but not efficiently

--- solver.py
+++ solver.py
@@ -1,5 +1,6 @@
 import numpy
 import sys
+import matplotlib as mpl
 import matplotlib.pyplot as pyplot
 
 if (len(sys.argv) != 2):
@@ -12,9 +13,11 @@ dat    = numpy.loadtxt(citiesdata, delimiter=',', skiprows=1)
 numberofcities = len(dat)
 
 def distance(city1, city2):
+    "Calculates the distance between two given cities."
     return numpy.sqrt((city1[0] - city2[0]) ** 2 + (city1[1] - city2[1]) ** 2)
 
 def divideaxis(data):
+    "Make separate list for x and y axis points"
     xaxis, yaxis = [], []
     xindex, yindex = [], []
     for i in range(len(data)):
@@ -25,6 +28,7 @@ def divideaxis(data):
     return (xaxis, yaxis)
 
 def divideCities(data):
+    "Divides cities into South and North region"
     midpoint = sum(divideaxis(data)[1]) / len(divideaxis(data)[1])
     
     below, above = [], []
@@ -37,15 +41,18 @@ def divideCities(data):
     return(below, above)
 
 def check(data, start):
-    for i in range(start, len(data)-2):
+    "Checks if there are closer city than the one currently on the next list"
+    for i in range(start, len(data)-3):
         if distance(data[i], data[i+1]) > distance(data[i], data[i+2]):
-            chikai = data[i+2]
-            tooi = data[i+1]
-            data[i+1] = chikai
-            data[i+2] = tooi
+            if distance(data[i+1], data[i+3]) < distance(data[i+2], data[i+3]):
+                chikai = data[i+2]
+                tooi = data[i+1]
+                data[i+1] = chikai
+                data[i+2] = tooi
     return data
 
 def connectBelow(below, ordered = []):
+    "Sort Southern cities into order"
     xaxis = divideaxis(below)[0]
     yaxis = divideaxis(below)[1]
     
@@ -59,6 +66,7 @@ def connectBelow(below, ordered = []):
     return ordered
 
 def connectAbove(above, ordered):
+    "Sort Northern cities into order"
     xaxis = divideaxis(above)[0]
     yaxis = divideaxis(above)[1]
     start = len(ordered)
@@ -73,6 +81,7 @@ def connectAbove(above, ordered):
     return ordered
 
 def totaldistance(ordered):
+    "Calculates distance to travel in order of 'ordered' cities"
     traveldistance = 0
     for i in range(len(ordered)):
         try:
@@ -91,7 +100,13 @@ distance = totaldistance(cities)
 print distance
 
 def graph(cities):
+    "Locations of the cities"
     pyplot.figure()
+    cmap = mpl.cm.cool
     for i in range(len(cities)):
         pyplot.plot(cities[i][0], cities[i][1], "o")
+        try:
+            pyplot.plot((cities[i][0], cities[i+1][0]), (cities[i][1],cities[i+1][1]), "-", color=cmap(i / float(len(cities))))
+        except IndexError:
+            pyplot.plot((cities[i][0], cities[0][0]), (cities[i][1],cities[0][1]), "-", color='purple')
     pyplot.show()
